666bede4348a02ce2e7f02cbb7fd74f48001ec9e
rahatkabir007 <rahatkabir04@gmail.com>
2024-05-28 23:20:25 +0600

    multiple file upload

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,23 +1,26 @@
-import { Controller, Post, UploadedFile, UseInterceptors, Req, BadRequestException } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Controller, Post, UploadedFiles, UseInterceptors, Req, BadRequestException } from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
 import { diskStorage } from 'multer';
 import { Request } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 @Controller('upload')
 export class UploadController {
     constructor(private readonly uploadService: UploadService) { }
 
     @Post('image')
-    @UseInterceptors(FileInterceptor('image', {
+    @UseInterceptors(FilesInterceptor('image', 20, {
         storage: diskStorage({
             destination: './uploads',
             filename: (req, file, cb) => {
-                cb(null, `${file.originalname}`);
+                // const uniqueName = `${uuidv4()}-${file.originalname}`;
+                // cb(null, uniqueName);
+                cb(null, file.originalname);
             },
         }),
         limits: {
-            fileSize: 5000000, // limit file size to 2MB
+            fileSize: 5000000, // limit file size to 5MB
         },
         fileFilter: (req, file, cb) => {
             if (!file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
@@ -27,9 +30,12 @@ export class UploadController {
             cb(null, true);
         },
     }))
-    async uploadImage(@UploadedFile() file, @Req() req: Request) {
-        const url = `${process.env.API_URL}/${file.filename}`;
-        console.log(url);
-        return { url };
+    async uploadImage(@UploadedFiles() files, @Req() req: Request) {
+        const response = files.map(file => {
+            const url = `${process.env.API_URL}/${file.originalname}`;
+            return { id: file.originalname.split('-')[0], url };
+        });
+        // console.log(response);
+        return response;
     }
 }
